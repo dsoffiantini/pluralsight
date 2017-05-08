5077ab8b816773e26e09bcb3f07af593ce161aad
dsoffiantini <dsoffiantini@gmail.com>
2017-05-08 11:50:54 -0600

    Convert 2D Arrays passes test cases

--- convertTwoDimensionalArrayToObject.js
+++ convertTwoDimensionalArrayToObject.js
@@ -1,5 +1,27 @@
-let arrayToObject = () => {
-    return false;
+let arrayToObject = (arrayOfArrays) => {
+    if (!Array.isArray(arrayOfArrays)) return false;
+
+    let allItemsAreArrays = true;
+    for (let i = 0; i < arrayOfArrays.length; i++) {
+        if (!Array.isArray(arrayOfArrays[i])) {
+            allItemsAreArrays = false;
+            break;
+        }
+    }
+
+    if (allItemsAreArrays) {
+        let packageObject = arrayOfArrays.reduce((service, [key, value]) => {
+            service[key] = value;
+            return service;
+        }, {})
+        return packageObject;
+    }
+
+    else {
+        return false;
+    }
+
+
 };
 
 export default arrayToObject;
