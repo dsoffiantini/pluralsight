a11e3705a160f4b332812acc505cbbd5499e9880
dsoffiantini <dsoffiantini@gmail.com>
2017-05-09 07:23:30 -0600

    Created Package Installer function that uses all other functions together.

--- packageInstaller.js
+++ packageInstaller.js
@@ -1,5 +1,29 @@
-let packageInstaller = () => {
-    return;
+import arraySplitter from './arraySplitter';
+import convertTwoDimensionalArrayToObject from './convertTwoDimensionalArrayToObject';
+import installPackagesFromObject from './installPackagesFromObject';
+
+let packageInstaller = (packageArray) => {
+    let formattedPackageArray = arraySplitter(packageArray);
+    let packageObject;
+    let packageOrder;
+
+    if (formattedPackageArray === false) {
+        return false;
+    } else {
+        packageObject = convertTwoDimensionalArrayToObject(formattedPackageArray);
+    }
+
+    if (packageObject === false) {
+        return false;
+    } else {
+        packageOrder = installPackagesFromObject(packageObject);
+    }
+
+    if (packageOrder === false) {
+        return false;
+    } else {
+        return packageOrder;
+    }
 };
 
 export default packageInstaller;
